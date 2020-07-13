7b2199d140f8f392f641e7a380da21243cc3e2b7
urameshiya <20288123+urameshiya@users.noreply.github.com>
2020-07-13 19:15:47 -0400

    Clean up ignored files

---
+++ ZealousTests/ZealousTests.swift
@@ -0,0 +1,33 @@
+//
+//  ZealousTests.swift
+//  ZealousTests
+//
+//  Created by Chinh Vu on 6/24/20.
+//  Copyright © 2020 urameshiyaa. All rights reserved.
+//
+
+import XCTest
+
+class ZealousTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
